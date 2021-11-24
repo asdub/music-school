4c72bc58292552e6a54817c3bcae250c6ce0f7bb
ASC Studio <asc@adamsmith.ie>
2021-11-24 10:50:56 +0000

    Updated contact form model to redirect back to home after submission

--- contact/models.py
+++ contact/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.shortcuts import redirect
 import django.forms
 
 from modelcluster.fields import ParentalKey
@@ -8,6 +9,7 @@ from wagtail.admin.edit_handlers import (
     InlinePanel,
     MultiFieldPanel
 )
+from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.contrib.forms.forms import FormBuilder
 from wagtail.contrib.forms.models import (
@@ -42,14 +44,25 @@ class ContactForm(AbstractEmailForm):
     thank_you_text = RichTextField(blank=True)
 
     content_panels = AbstractEmailForm.content_panels + [
-        FieldPanel('intro'),
+        FieldPanel('intro', heading="Form intro text"),
         InlinePanel('form_fields', label='Form Fields'),
-        FieldPanel('thank_you_text'),
+        FieldPanel('thank_you_text', heading="Form submission/ thank you text"),
         MultiFieldPanel([
             FieldRowPanel([
                 FieldPanel('from_address', classname="col6"),
                 FieldPanel('to_address', classname="col6"),
             ]),
-            FieldPanel("subject"),
+            FieldPanel('subject'),
         ], heading="Email Settings"),
     ]
+
+    def render_landing_page(self, request, form_submission=None, *args, **kwargs):
+        source_page_id = request.POST.get('source-page-id')
+        source_page = Page.objects.get(pk=source_page_id)
+
+        if source_page:
+            request.session['form_page_success'] = True
+            return redirect(source_page.url  + '#contact-us', permanent=False)
+
+        # if no source_page is set, render default landing page
+        return super().render_landing_page(request, form_submission, *args, **kwargs)
